0876cb0facc3e9e084b81c9928154c6231547610
krushna Durole <krushnadurole2000ad@gmail.com>
2022-12-06 15:57:41 +0530

    update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,49 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
+import DevContext from '../context/developers/DevContext';
 import "./Navbar.css"
 const Navbar = () => {
     const navigate = useNavigate();
+    useEffect(() => {
+        getuser();
+    }, [])
+    
     const handleLogOut = () => {
         if(localStorage.getItem('authtoken')){
             localStorage.removeItem("authtoken")
             navigate('/login')
         }    
     }
+    const [user, setuser] = useState({ id: "", ename: "", eemail: "" });
+    // const context = DevContext;
+    // const {user}  = context;
     let location = useLocation();
+    const getuser = async () => {
+        const response = await fetch('https://developerrvit.onrender.com/api/v1/veryhelpful', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'authtoken': localStorage.getItem('authtoken')
+          },
+          body: JSON.stringify()
+        })
+        const json = await response.json();
+        console.log(json);
+        setuser(json);
+      }
     return (
         <>
 
             <div className=''>
                 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div class="container-fluid">
-                        <a class="navbar-brand " href="#">DEV_GRAM</a>
+                        <a class="navbar-brand " href="#">DEV_GRAM </a><span className='email'>{user.email}</span>
                     </div>
                 </nav>
                 <div className='Hello navi tvft'>
                     <div class="btn-group name">
                         <button class="btn btn-secondary btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Developer
+                            Developer                         
                         </button>
                         <ul class="dropdown-menu mennu">
                             {/* <Link className="btn btn-primary mx-1 button" to="/">Developer </Link> */}
@@ -36,15 +57,15 @@ const Navbar = () => {
                         </button>
                         <ul class="dropdown-menu mennu ">
                             {!localStorage.getItem('authtoken') ? <Link className="btn btn-primary mx-1 button" to="/login" role="button">Log In</Link>
-                            :<button type="button" class="btn btn-warning" onClick={handleLogOut}> Log out</button>}
+                            :<button type="button" class="btn btn-warning buttonn" onClick={handleLogOut}> Log out</button>}
                             {!localStorage.getItem('authtoken')  && 
                                                         <Link className="btn btn-primary mx-1 button" to="/signup" role="button">Sign Up</Link>}
                                                         <br></br>
-                            <Link className="btn btn-primary mx-1 button" to="/aboutus" role="button">About us</Link>
-                            <Link className="btn btn-primary mx-1 button" to="/" role="button">User Profile</Link>  </ul>
+                            <Link className="btn btn-primary mx-1 button trial" to="/aboutus" role="button">About us</Link>
+                            <Link className="btn btn-primary mx-1 button trial" to="/" role="button">User Profile</Link>  </ul>
                     </div>
                     <div class="btn-group name">
-                        <button class="btn btn-secondary btn-info dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" type="button">
+                        <button class="btn btn-secondary btn-info dropdown-toggle bnt" data-bs-toggle="dropdown" aria-expanded="false" type="button">
                             Requirements
                         </button>
                         <ul class="dropdown-menu mennu">
